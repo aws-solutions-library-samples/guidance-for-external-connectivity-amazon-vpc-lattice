7661e6fca5fa9c3ac5f49ab5ab7cf3c7ec2a9a1f
Adam Palmer <adampalm@amazon.co.uk>
2024-03-05 13:52:45 +0000

    updates to scripts

--- callendpoint.sh
+++ callendpoint.sh
@@ -2,8 +2,8 @@
 
 #Use this script to make a signed request to your endpoint using the inbuilt sigv4 mechanism in curl.
 
-curl -4 https://hello.palmer-solutions.co.uk \
-    --aws-sigv4 "aws:amz:eu-west-1:vpc-lattice-svcs" \
+curl -4 %yourendpoint% \
+    --aws-sigv4 "aws:amz:%region%:vpc-lattice-svcs" \
     --user "$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" \
     --header "x-amz-security-token:$AWS_SESSION_TOKEN" \
     --header "x-amz-content-sha256:UNSIGNED-PAYLOAD"
